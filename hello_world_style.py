edd305e9f5680183852fe65d4360e8fce63a5b90
Ubuntu <ubuntu@ip-172-31-18-62.ec2.internal>
2021-04-27 18:00:50 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Jonathan Kene
+# Date: April 27, 2021
+# This program displays "Hello, World!" and follows correct
+# Python Style
+
+def main():
+    print("Hello, World! with proper style!")
+
+
+if __name__ == "__main__":
+    main()
